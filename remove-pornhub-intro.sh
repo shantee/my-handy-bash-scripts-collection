58e2dad199d890db15e37f613bca0990dfa91eba
Philippe BENOIST <shantee@users.noreply.github.com>
2021-10-16 14:18:14 +0200

    better automation

--- remove-pornhub-intro.sh
+++ remove-pornhub-intro.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-mkdir -p out
+mkdir -p tmp
 
 for i in *.mp4; do
-ffmpeg -i "$i" -ss 3 -vcodec copy -acodec copy out/"$i"
+mv "$i" ./tmp/"$i"
 done
+echo -e "\n\n !! mp4 files have been temporarely moved to tmp folder !! \n"
+
+cd tmp 
+sleep 3
+for i in *.mp4; do
+echo -e "removing intro from : $i"
+ffmpeg -hide_banner -loglevel error -i "$i" -ss 3 -vcodec copy -acodec copy -y ../"$i"
+
+done
+echo -e "\n done ! \n"
+echo -e "all files have been trated and copied to origin folder\n"
+echo -e "\nnow you can delete the folder tmp"
